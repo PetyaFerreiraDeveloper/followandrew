98fe5186bf73d3791d8f90c0d93efb64f012a09d
Petya Naydenova Ferreira <pn@abtion.com>
2025-01-03 14:40:40 +0100

    add the category to the article template

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -8,7 +8,15 @@
       ?>
 
       <?php
-      the_category('<span class="tag"><i class="fa fa-tag"></i>', 'single');
+      $categories = get_the_category();
+      if (!empty($categories)) {
+        foreach ($categories as $category) {
+          echo '<span class="tag"><i class="fa fa-folder"></i> ';
+          echo '<a href="' . esc_url(get_category_link($category->term_id)) . '" class="category-link">';
+          echo esc_html($category->name);
+          echo '</a></span>';
+        }
+      }
       ?>
       <span class="comment">
         <a href="#comments"><i class='fa fa-comment'></i>
